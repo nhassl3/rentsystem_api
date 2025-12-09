47eefda6f48208b2cff29ea4a6d28f6faca9ceb9
nhassl3 <matvey.kvasov.05@mail.ru>
2025-12-09 22:32:39 +0300

    update validating of car proto image path

--- generated/go/rentsystem/CarOptions.pb.validate.go
+++ generated/go/rentsystem/CarOptions.pb.validate.go
@@ -89,18 +89,7 @@ func (m *CarData) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
-	if err := m._validateHostname(m.GetImagePath()); err != nil {
-		if ip := net.ParseIP(m.GetImagePath()); ip == nil {
-			err := CarDataValidationError{
-				field:  "ImagePath",
-				reason: "value must be a valid hostname, or ip address",
-			}
-			if !all {
-				return err
-			}
-			errors = append(errors, err)
-		}
-	}
+	// no validation rules for ImagePath
 
 	if all {
 		switch v := interface{}(m.GetEngine()).(type) {
@@ -220,36 +209,6 @@ func (m *CarData) validate(all bool) error {
 
 	if len(errors) > 0 {
 		return CarDataMultiError(errors)
-	}
-
-	return nil
-}
-
-func (m *CarData) _validateHostname(host string) error {
-	s := strings.ToLower(strings.TrimSuffix(host, "."))
-
-	if len(host) > 253 {
-		return errors.New("hostname cannot exceed 253 characters")
-	}
-
-	for _, part := range strings.Split(s, ".") {
-		if l := len(part); l == 0 || l > 63 {
-			return errors.New("hostname part must be non-empty and cannot exceed 63 characters")
-		}
-
-		if part[0] == '-' {
-			return errors.New("hostname parts cannot begin with hyphens")
-		}
-
-		if part[len(part)-1] == '-' {
-			return errors.New("hostname parts cannot end with hyphens")
-		}
-
-		for _, r := range part {
-			if (r < 'a' || r > 'z') && (r < '0' || r > '9') && r != '-' {
-				return fmt.Errorf("hostname parts can only contain alphanumeric characters or hyphens, got %q", string(r))
-			}
-		}
 	}
 
 	return nil
